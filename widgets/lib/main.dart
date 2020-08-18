d45c5c5a89883d6060c5b62a95d1887fca6969c9
Kareem Abbasi <kareem.abbasi@mail.huji.ac.il>
2020-08-18 22:31:55 +0300

    changed to material design

--- widgets/lib/main.dart
+++ widgets/lib/main.dart
@@ -62,9 +62,41 @@ class MyScaffold extends StatelessWidget {
   }
 }
 
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
+}
+
 void main() {
-  runApp(MaterialApp(
-    title: 'My App',
-    home: MyScaffold(),
-  ));
+  runApp(MaterialApp(title: 'Flutter Tutorial', home: TutorialHome()));
+  // runApp(MaterialApp(
+  //   title: 'My App',
+  //   home: MyScaffold(),
+  // ));
 }
